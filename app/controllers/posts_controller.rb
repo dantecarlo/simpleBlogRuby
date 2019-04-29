e2b3d2b0c96499759de68510d18ed42eeacd2f04
Dante Nuñez <dante@Dantes-MacBook-Pro.local>
2019-04-29 16:56:16 -0500

    Add Single post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
 
+    def show
+        @post = Post.find(params[:id])
     end
 
     def new
@@ -8,6 +12,14 @@ class PostsController < ApplicationController
     end
 
     def create
-        render plain: params[:post].inspect
+        #render plain: params[:post].inspect
+        @post = Post.new(params[post_params])
+
+        @post.save
+        redirect_to @post
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
